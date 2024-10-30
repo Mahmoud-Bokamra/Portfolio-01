0815ef9fae136b9a994d7859deab2a7dc81e83d8
Mahmoud Abu-Qamar <m.m.abuqamar@gmail.com>
2024-10-30 19:40:38 +0200

    disable eslint

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -72,8 +72,8 @@ const Contact = () => {
             <ul className='flex flex-col gap-10'>
               {info.map((item,index)=> {
                 return (
-                  <Link href={item.href}>
-                    <li key={index} className='flex items-center gap-6 '>
+                  <Link href={item.href} key={index} >
+                    <li className='flex items-center gap-6 '>
                       <div className='w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center'>
                         <div className='text-[28px]'>{item.icon}</div>
                       </div>
